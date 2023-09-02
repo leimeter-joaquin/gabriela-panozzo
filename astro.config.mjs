56669946242e19021614e7d019864d1287b18638
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-09-02 17:15:17 -0300

    storyblok integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,31 @@
-import { defineConfig } from 'astro/config';
-
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK_TOKEN");
+console.log("AAAAAAAAAA", env);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+
+      apiOptions: {
+        region: "us",
+      },
+      components: {
+        texto: "storyblok/Texto",
+      },
+    }),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="astro/client" />
+
+// TODO: remove this fix one this PR is impelemented in @storyblok/astro https://github.com/storyblok/storyblok-astro/pull/414
+declare module "@storyblok/astro/StoryblokComponent.astro";
