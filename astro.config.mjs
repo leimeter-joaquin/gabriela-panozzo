ddba3294a67fa12f30f5b58c032d6769eef8b752
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-09-04 19:04:40 -0300

    add preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 const env = loadEnv("", process.cwd(), "STORYBLOK_TOKEN");
 console.log("AAAAAAAAAA", env);
 
 // https://astro.build/config
 export default defineConfig({
+  output: process.env.PUBLIC_ENV === "preview" ? "server" : "static",
+  adapter: process.env.PUBLIC_ENV === "preview" ? vercel() : undefined,
   vite: {
     plugins: [basicSsl()],
     server: {
@@ -18,6 +22,8 @@ export default defineConfig({
   integrations: [
     tailwind(),
     storyblok({
+      bridge: process.env.PUBLIC_ENV !== "production",
+
       accessToken: env.STORYBLOK_TOKEN,
 
       apiOptions: {
