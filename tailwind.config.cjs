dcf0599cd123b70276e173def9b87b726c01ccbd
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-09-12 16:55:39 -0300

    asd

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,29 +10,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-  daisyui: {
-    themes: [
-      {
-        myTheme: {
-          primary: "#EBB65C",
-          secondary: "#E46F62",
-          accent: "#BC4E81",
-          neutral: "#E0F2E9",
-          "base-100": "#FDF6ED",
-          "base-content": "##241023",
-          info: "#3271e7",
-          success: "#47e68f",
-          warning: "#f8b359",
-          error: "#e36059",
-        },
-      },
-    ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-  },
+  plugins: [require("@tailwindcss/typography")],
 };
