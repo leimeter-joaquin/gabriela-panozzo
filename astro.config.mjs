bfae3791c1b79092e78c81a5d34f1abe25964f8c
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-10-15 16:47:27 -0300

    WIP form

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
-
 const env = loadEnv("", process.cwd(), ["STORYBLOK_TOKEN", "IS_PREVIEW"]);
-
 const output = env.IS_PREVIEW === "true" ? "server" : "static";
 const adapter = env.IS_PREVIEW === "true" ? vercel() : undefined;
 
@@ -25,9 +22,7 @@ export default defineConfig({
     tailwind(),
     storyblok({
       bridge: env.IS_PREVIEW,
-
       accessToken: env.STORYBLOK_TOKEN,
-
       apiOptions: {
         region: "us",
       },
